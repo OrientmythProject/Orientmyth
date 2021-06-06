c162cb460361828be1f2f69928899c33b72f8837
cyciling <1377899390@qq.com>
2021-06-06 18:23:34 +0800

    update version

--- src/main/java/com/foxstudio/orientmyth/Orientmyth.java
+++ src/main/java/com/foxstudio/orientmyth/Orientmyth.java
@@ -19,7 +19,7 @@ import org.apache.logging.log4j.Logger;
 public class Orientmyth {
     public static final String MOD_ID = "orientmyth";
     public static final String NAME = "Orientmyth";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "0.0.1";
     public static final String DEPENDENCIES = "required-after:bookshelf;required-after:baubles";
 
     public static Logger logger;
